4f11ffe64095e39f1159332edb01ccfacdb9607b
Lucas Pimenta <lucas.pimenta94@outlook.com>
2017-10-04 14:25:57 -0300

    First commit Fluig SaveForm Extension for Chrome

---
+++ saveform.js
@@ -0,0 +1,80 @@
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    };
+  
+    chrome.tabs.query(queryInfo, (tabs) => {
+      // chrome.tabs.query invokes the callback with a list of tabs that match the
+      // query. When the popup is opened, there is certainly a window and at least
+      // one tab, so we can safely assume that |tabs| is a non-empty array.
+      // A window can only have one active tab at a time, so the array consists of
+      // exactly one tab.
+      var tab = tabs[0];
+  
+      // A tab is a plain object that provides information about the tab.
+      // See https://developer.chrome.com/extensions/tabs#type-Tab
+      var url = tab.url;
+  
+      // tab.url is only available if the "activeTab" permission is declared.
+      // If you want to see the URL of other tabs (e.g. after removing active:true
+      // from |queryInfo|), then the "tabs" permission is required to see their
+      // "url" properties.
+      console.assert(typeof url == 'string', 'tab.url should be a string');
+  
+      callback(url);
+    });
+}
+
+/**
+ * Remove all block tags from the document.
+ */
+function removeBlockTags() {
+    //var script = 'document.body.style.backgroundColor="' + color + '";';
+
+    var script = `  var blocks = document.getElementsByClassName("blockUI");
+                    for (var i = blocks.length; i--;) {
+                        blocks[i].remove();
+                    }
+                    alert("Possivelmente a sua sessão está expirada, por favor faça login, volte para esta aba e salve o formulário.");
+                    window.open(window.location.origin, "_blank");
+                    `;
+    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
+    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
+    // into a page. Since we omit the optional first argument "tabId", the script
+    // is inserted into the active tab of the current window, which serves as the
+    // default.
+    chrome.tabs.executeScript({
+      code: script
+    });
+}
+  
+
+  // This extension loads the saved background color for the current tab if one
+// exists. The user can select a new background color from the dropdown for the
+// current page, and it will be saved as part of the extension's isolated
+// storage. The chrome.storage API is used for this purpose. This is different
+// from the window.localStorage API, which is synchronous and stores data bound
+// to a document's origin. Also, using chrome.storage.sync instead of
+// chrome.storage.local allows the extension data to be synced across multiple
+// user devices.
+document.addEventListener('DOMContentLoaded', () => {
+    getCurrentTabUrl((url) => {
+        var salvarForm = document.getElementById('salvarForm');
+        
+        // when the dropdown selection changes.
+        salvarForm.addEventListener('click', () => {
+           removeBlockTags();     
+        });
+    });
+});
+  
